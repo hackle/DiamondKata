1bb35e467bd4f1ef8765800c9f879f1402892354
Hackle Wayne <hacklew@hotmail.com>
2016-08-13 23:28:46 +1200

    Have vertical

---
+++ Fata/Fata/Diamond.fs
@@ -0,0 +1,10 @@
+﻿namespace Fata
+
+module Dimaond =
+    let diamond (letter: char) = 
+        let vertical = 
+            [ (int 'A') .. (int letter)] @ [(int letter) - 1 .. -1 .. (int 'A')]
+            |> List.map char
+            |> List.map string
+            |> System.String.Concat
+        vertical
---
+++ Fata/Fata/DiamondTests.fs
@@ -0,0 +1,17 @@
+﻿module FataTests
+
+open Fata.Dimaond
+open Xunit
+open Swensen.Unquote
+
+[<Theory>]
+[<InlineData('A', "A")>]
+let ``For 'A' the diamond is just 'A'`` (letter: char) (expected: string) = 
+    let actual = diamond letter
+    Assert.Equal(actual, expected)
+     
+[<Theory>]
+[<InlineData('B', "ABA")>]
+let ``For 'B' I have ABA vertically`` (letter: char) (expected: string) = 
+    let actual = diamond letter
+    Assert.Equal(actual, expected)
