f6bdd57511de21eacc25d8b403d3713d66632f35
Hackle Wayne <hacklew@hotmail.com>
2016-08-15 21:43:52 +1200

    can rotate lists

--- Fata/Fata/Diamond.fs
+++ Fata/Fata/Diamond.fs
@@ -5,6 +5,5 @@ module Dimaond =
         let vertical = 
             [ (int 'A') .. (int letter)] @ [(int letter) - 1 .. -1 .. (int 'A')]
             |> List.map char
-            |> List.map string
-            |> System.String.Concat
+            |> List.map (fun x -> [ x ])
         vertical
--- Fata/Fata/DiamondTests.fs
+++ Fata/Fata/DiamondTests.fs
@@ -4,14 +4,48 @@ open Fata.Dimaond
 open Xunit
 open Swensen.Unquote
 
-[<Theory>]
-[<InlineData('A', "A")>]
-let ``For 'A' the diamond is just 'A'`` (letter: char) (expected: string) = 
-    let actual = diamond letter
-    Assert.Equal(actual, expected)
+[<Fact>]
+let ``For 'A' the diamond is just 'A'`` () = 
+    let actual = diamond 'A'
+    let expected = [ [ 'A' ] ]
+    Assert.Equal<char list list>(actual, expected)
      
-[<Theory>]
-[<InlineData('B', "ABA")>]
-let ``For 'B' I have ABA vertically`` (letter: char) (expected: string) = 
-    let actual = diamond letter
-    Assert.Equal(actual, expected)
+[<Fact>]
+let ``For 'B' I have ABA vertically`` () = 
+    let actual = 
+        diamond 'B' 
+        |> List.map (fun l -> l |> List.filter (fun x -> x <> ' '))
+    let expected = [ [ 'A' ]; ['B']; ['A'] ]
+    Assert.Equal<char list list>(actual, expected)
+
+let splitHead ls = 
+    match ls with
+    | h::t -> h, t
+    | _ -> failwith "must have at least 2 element"
+
+let takeAllHead ls =
+    ls |> List.map (fun item ->
+                        let (l, r) = item
+                        l)
+let takeAllTail ls =
+    ls |> List.map (fun item ->
+                        let (l, r) = item
+                        r)
+
+let rec folder (ls: 'a list) (carry: 'a list list)  =
+    match carry, ls with
+    | lh::ltail, h::tail -> (h::lh) :: (folder tail ltail)
+    | _ -> carry
+
+let rotate lists = 
+    List.foldBack folder lists (lists.Head |> List.map (fun l -> List.empty<char>))
+
+[ [ 'A'; 'B'; 'C' ]; [ 'D'; 'E'; 'F' ]]
+|> rotate
+
+[<Fact>]
+let ``For 'B' I have 'BAB' horozontally`` () =
+    let actual =
+        diamond 'B'
+        |> slice
+        |> 
